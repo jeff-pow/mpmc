677a7c93861c9c19597cec5adb14fc0b842973ac
Jeff Powell <jrpowel9@ncsu.edu>
2023-04-23 11:43:23 -0400

    .

--- shell.nix
+++ shell.nix
@@ -13,9 +13,7 @@ stdenv.mkDerivation {
     shellHook = ''
         export PATH=$PATH:${pkgs.cudaPackages.cudatoolkit}/bin
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.cudaPackages.cudatoolkit}/lib64
-        export PATH=$PATH:${pkgs.cudaPackages.libcublas}/bin
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.cudaPackages.libcublas}/lib64
-        export PATH=$PATH:${pkgs.blas}/bin
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.blas}/lib64
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.lapack}/lib64
         '';
