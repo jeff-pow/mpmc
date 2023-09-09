a8faa4970431c39a487cd6e3f1fceb6f1cef92ad
Jeff Powell <jrpowel9@ncsu.edu>
2023-09-08 21:42:10 -0400

    .

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
     {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
+            clang-tools
             lapack
             cudaPackages.cudatoolkit
             cmake
