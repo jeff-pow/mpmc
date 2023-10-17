0f5991a63492cb3a524baf1d182a28b1e0e7aeb5
Jeff Powell <jrpowel9@ncsu.edu>
2023-10-17 13:17:24 -0400

    .

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "MPMC Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { 
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+            clang-tools
+            lapack
+            cudaPackages.cudatoolkit
+            cmake
+            cudaPackages.libcublas
+            gcc
+            blas
+            valgrind
+
+            python3
+            python311Packages.python-lsp-server
+            python311Packages.autopep8
+            python310Packages.ujson
+            python310Packages.numpy
+            python310Packages.scipy
+            python310Packages.pluggy
+            python310Packages.docstring-to-markdown
+            python310Packages.jedi
+        ];
+
+        shellHook = ''
+        '';
+      };
+    });
+}
